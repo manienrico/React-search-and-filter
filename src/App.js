a961f356f8f78c3b7a1c5844d8669044934b0cfe
Enrico Mani <manienrico13@gmail.com>
2023-05-29 12:07:47 +0300

    Implementation of functional components

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import './App.css';
 
 const App =()=>{
   console.log('render')
-  const [searchField,setSearchField] = useState('a')
+  const [searchField,setSearchField] = useState('')
+  const [title, setTitle] = useState('')
   const[monsters,setMonsters] = useState([])
   const [filteredMonsters,setFilteredMonsters] = useState(monsters)
 
@@ -33,14 +34,25 @@ const App =()=>{
     setSearchField(searchFieldString)
   }
 
+  const onTitleChange = (event)=>{
+    const searchFieldString = event.target.value.toLocaleLowerCase()
+    setTitle(searchFieldString)
+  }
+
   return(
     <div>
-      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'>{title}</h1>
 
       <SearchBox 
         onChangeHandler={onSearchChange} 
         placeholder='search monsters'
         className='monsters-search-box'
+      />
+      <br />
+      <SearchBox 
+        onChangeHandler={onTitleChange} 
+        placeholder='set title'
+        className='title-search-box'
       />
 
       <CardList monsters={filteredMonsters} />
